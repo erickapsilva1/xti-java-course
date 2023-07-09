b15b0238363ececae5c74fa82d1a834281681cb9
Erick Silva <erick.ap.silva@gmail.com>
2023-07-09 16:16:08 -0300

    working with constants

---
+++ Constantes.java
@@ -0,0 +1,22 @@
+/**
+* Apresentar os conceitos de constantes
+* @author: Erick A. da Silva
+*/
+public class Constantes {
+	public static void main(String[] args){
+		
+		// variavel
+		int populacaoBrasileira = 203062512;
+		
+		// constante
+		final double PI = 3.1415926535;
+		// pi = 123; // < -- erro
+		
+		final char = GENERO_MASCULINO = 'M';
+		final char = GENERO_FEMININO = 'F';
+		final char = GENERO_OUTROS = 'O';
+		
+		System.out.println(pi);
+		
+	}
+}
