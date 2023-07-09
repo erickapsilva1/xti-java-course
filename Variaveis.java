eca98ea36b7b262d77af855110510d1625d27757
Erick Silva <erick.ap.silva@gmail.com>
2023-07-09 15:33:09 -0300

    working with variables

---
+++ Variaveis.java
@@ -0,0 +1,18 @@
+/**
+* Apresentar os conceitos de variaveis
+* @author: Erick A. da Silva
+*/
+public class Variaveis {
+	
+	public static void main(String[] args){
+		
+		String nome = "Erick";
+		String nome2;
+		String nome3, sobreNome, ultimoNome;
+		int idade = 32;
+		boolean casado = true;
+		
+		System.out.println(nome);
+	}
+	
+}
