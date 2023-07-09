6c4b54ebd33c5324795eb193426d6e8b5510bf22
Erick Silva <erick.ap.silva@gmail.com>
2023-07-09 17:21:46 -0300

    input methods - args and Scanner

---
+++ Entrada.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Entrada {
+	
+	public static void main(String[] args){
+		
+		// Chamada do programa
+		// System.out.println(args[0]);
+		
+		// Interagindo com o usuario
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Type your name: ");
+		String name = scanner.nextLine();
+		System.out.println("Welcome " + name);
+		
+	}
+}
