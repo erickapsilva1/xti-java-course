38c3664b65d0e1cc945538f5f4a9a57c18d14ee9
Erick Silva <erick.ap.silva@gmail.com>
2023-07-10 13:34:51 -0300

    assignment operators

---
+++ OperadoresAtribuicao.java
@@ -0,0 +1,15 @@
+public class OperadoresAtribuicao {
+	public static void main(String[] args){
+
+		int x = 6;
+		x += 3; // equals to x = x + 3
+		
+		System.out.println(x);
+		
+		int y = 9;
+		y *= 3;
+		
+		System.out.println(y);
+
+	}
+}
