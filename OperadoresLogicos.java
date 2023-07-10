f3ea427e7411511da2233b873f16534ec59cd1b0
Erick Silva <erick.ap.silva@gmail.com>
2023-07-10 12:00:56 -0300

    comparison operators

---
+++ OperadoresLogicos.java
@@ -0,0 +1,14 @@
+public class OperadoresLogicos {
+	public static void main(String[] args){
+		int x = 6;
+		System.out.println(x == 6);
+		System.out.println(x == 7);
+		// System.out.println(x == "8"); <-- compile error
+		System.out.println(x != 7);
+		System.out.println(x > 7);
+		System.out.println(x < 7);
+		System.out.println(x >= 7);
+		System.out.println(x <= 7);
+		
+	}
+}
