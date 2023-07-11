0afaa915750b0b35b8eb972e3b0075b992e36864
Erick Silva <erick.ap.silva@gmail.com>
2023-07-11 08:00:16 -0300

    working with switch clause

---
+++ FlowII.java
@@ -0,0 +1,21 @@
+public class FlowII {
+	public static void main(String[] args){
+		
+		char gender = 'F';
+		
+		switch(gender){
+			case 'M':
+				System.out.println("Male");
+				break;
+			case 'F':
+				System.out.println("Famele");
+				break;
+			case 'O':
+				System.out.println("Other");
+				break;
+			default:
+				System.out.println("Uninformed");
+		}
+		
+	}
+}
