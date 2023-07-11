375de55a933cb13b763b13c5b28d1be98a6d5ee6
Erick Silva <erick.ap.silva@gmail.com>
2023-07-11 08:33:17 -0300

    roll the dice and guess challenge

---
+++ RollDiceChallenge.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RollDiceChallenge {
+	public static void main(String[] args){
+		
+		Random random = new Random();
+		Scanner scanner = new Scanner(System.in);
+		
+		int roll = random.nextInt(6);
+		
+		System.out.println("Rolled dice! What's your guess?");
+		
+		int guess = scanner.nextInt();
+		
+		System.out.println("Roll: " + roll + ", " + "Your guess: " + guess);
+		
+		if(roll == guess){
+			System.out.println("You won!");
+		}else{
+			System.out.println("You lose!");
+		};
+		
+	}
+}
