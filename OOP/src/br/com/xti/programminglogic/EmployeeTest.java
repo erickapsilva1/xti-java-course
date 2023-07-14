e694c420f231422ee47f2b9041831ba5f3ac3a0f
Erick Silva <erick.ap.silva@gmail.com>
2023-07-14 17:21:32 -0300

    working with getters and setters

---
+++ OOP/src/br/com/xti/programminglogic/EmployeeTest.java
@@ -0,0 +1,13 @@
+package br.com.xti.programminglogic;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		
+		Employee employee = new Employee();
+		employee.setName("Assis");
+		System.out.println(employee.getName());
+
+	}
+
+}
