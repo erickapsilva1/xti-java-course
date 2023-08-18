c0a0a3918f90ba9cb8f3549785339590b4f96939
Erick Silva <erick.ap.silva@gmail.com>
2023-08-18 07:35:51 -0300

    JDBC - inserting multi data

--- src/br/com/xti/jdbc/InsertData.java
+++ src/br/com/xti/jdbc/InsertData.java
@@ -8,11 +8,19 @@ public class InsertData {
     public static void main(String[] args) {
 
         String url = "jdbc:oracle:thin:@localhost:1521:xe";
-        String sql = "INSERT INTO PESSOA VALUES (3, 'Hermione', 'F', 'hermione@xti.com.br')";
+        String sql = "INSERT INTO PESSOA VALUES (?, ?, ?, ?)";
+        String[] people = {"How", "Sheldon", "Leonard", "Rajesh", "Howard"};
 
         try(Connection connection = DriverManager.getConnection(url, "xti", "xti123");
         PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.executeUpdate();
+            for(int i = 0; i < people.length; i++){
+                preparedStatement.setInt(1, i + 4);
+                preparedStatement.setString(2, people[i]);
+                preparedStatement.setString(3, "M");
+                preparedStatement.setString(4, people[i].toLowerCase() + "@xti.com.br");
+                preparedStatement.addBatch();
+            }
+            preparedStatement.executeBatch();
         } catch (Exception exception){}
     }
 }
