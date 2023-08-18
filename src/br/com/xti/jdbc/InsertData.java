1834a2500ae439e2fe3460ae84fd691434e839c7
Erick Silva <erick.ap.silva@gmail.com>
2023-08-18 07:50:45 -0300

    JDBC - inserting multi data

--- src/br/com/xti/jdbc/InsertData.java
+++ src/br/com/xti/jdbc/InsertData.java
@@ -1,8 +1,6 @@
 package br.com.xti.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 
 public class InsertData {
     public static void main(String[] args) {
@@ -11,16 +9,24 @@ public class InsertData {
         String sql = "INSERT INTO PESSOA VALUES (?, ?, ?, ?)";
         String[] people = {"How", "Sheldon", "Leonard", "Rajesh", "Howard"};
 
-        try(Connection connection = DriverManager.getConnection(url, "xti", "xti123");
-        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            for(int i = 0; i < people.length; i++){
-                preparedStatement.setInt(1, i + 4);
-                preparedStatement.setString(2, people[i]);
-                preparedStatement.setString(3, "M");
-                preparedStatement.setString(4, people[i].toLowerCase() + "@xti.com.br");
-                preparedStatement.addBatch();
+        try(Connection connection = DriverManager.getConnection(url, "xti", "xti123")){
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                for (int i = 0; i < people.length; i++) {
+                    preparedStatement.setInt(1, i + 4);
+                    preparedStatement.setString(2, people[i]);
+                    preparedStatement.setString(3, "M");
+                    preparedStatement.setString(4, people[i].toLowerCase() + "@xti.com.br");
+                    preparedStatement.addBatch();
+                } preparedStatement.executeBatch();
+            }catch (SQLException exception) {}
+            sql = "SELECT NOME, EMAIL FROM PESSOA ORDER BY 1 ASC";
+
+            try(PreparedStatement preparedStatement1 = connection.prepareStatement(sql);
+                ResultSet resultSet = preparedStatement1.executeQuery(sql)){
+                while (resultSet.next()){
+                    System.out.println(resultSet.getString(1) + ": " + resultSet.getString(2));
+                }
             }
-            preparedStatement.executeBatch();
         } catch (Exception exception){}
     }
 }
